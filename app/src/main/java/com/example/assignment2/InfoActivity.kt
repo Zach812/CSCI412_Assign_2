0d4b511ec3aa77ff5e3b6e2fd294cc560e08c844
Zach <zach@weinzierl.us>
2024-09-05 19:03:29 -0500

    Adding Styling

--- app/src/main/java/com/example/assignment2/InfoActivity.kt
+++ app/src/main/java/com/example/assignment2/InfoActivity.kt
@@ -8,9 +8,12 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.icons.Icons
@@ -22,6 +25,7 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarColors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -34,88 +38,106 @@ import androidx.compose.ui.unit.sp
 class InfoActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val challenges = arrayOf(
+            "Device and OS Fragmentation",
+            "Unstable and Dynamic Environment",
+            "Rapid Changes",
+            "Tool Support",
+            "Low Tolerance"
+        )
         setContent {
-            TopBarNavigationExample()
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .fillMaxHeight()
+                    .background(Color(0xFF073926))
+            ) {
+                TopBarNavigation()
+                Challenges_List(challenges = challenges)
+            }
         }
     }
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun TopBarNavigationExample(
+fun TopBarNavigation(
 ) {
     val mContext = LocalContext.current
-    val challenges = arrayOf(
-        "Device and OS Fragmentation",
-        "Unstable and Dynamic Environment",
-        "Rapid Changes",
-        "Tool Support",
-        "Low Tolerance"
+    CenterAlignedTopAppBar(
+        colors = TopAppBarColors(
+            containerColor = Color(0xFF0a5640),
+            Color(0xFF0a5640),
+            Color(0xFFffc72a),
+            Color(0xFFffc72a),
+            Color(0xFFffc72a)
+        ),
+        title = {
+            Text(
+                "Software Engineering Challenges",
+                fontWeight = FontWeight.Bold,
+                color = Color(0xFFffc72a)
+            )
+        },
+        navigationIcon = {
+            IconButton(modifier = Modifier.size(size = 40.dp),onClick = {
+                mContext.startActivity(
+                    Intent(
+                        mContext,
+                        MainActivity::class.java
+                    )
+                )
+            }) {
+                Icon(
+                    imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                    contentDescription = "Localized description",
+                    modifier = Modifier.fillMaxSize()
+                )
+            }
+        },
     )
-    Column(
+
+}
+
+@Composable
+fun Challenges_List(challenges: Array<String>) {
+    LazyColumn(
+        verticalArrangement = Arrangement.SpaceEvenly,
+        horizontalAlignment = Alignment.CenterHorizontally,
         modifier = Modifier
             .fillMaxWidth()
-            .fillMaxHeight()
-            .background(Color(0xFF0a5640))
+            .fillMaxHeight(0.75f)
     ) {
-        CenterAlignedTopAppBar(
-            modifier = Modifier.background(Color(0xFF0a5640)),
-            title = {
-                Text(
-                    "Software Engineering Challenges",
-                    fontWeight = FontWeight.Bold,
-                    color = Color(0xFF0a5640)
-                )
-            },
-            navigationIcon = {
-                IconButton(onClick = {
-                    mContext.startActivity(
-                        Intent(
-                            mContext,
-                            MainActivity::class.java
-                        )
-                    )
-                }) {
-                    Icon(
-                        imageVector = Icons.AutoMirrored.Filled.ArrowBack,
-                        contentDescription = "Localized description"
-                    )
-                }
-            },
-        )
-        LazyColumn(
-            verticalArrangement = Arrangement.SpaceEvenly,
-            horizontalAlignment = Alignment.CenterHorizontally,
-            modifier = Modifier
-                .fillMaxWidth()
-                .fillMaxHeight(0.75f)
-        ) {
-            itemsIndexed(challenges) { index, challenge ->
-                Card(
-                    colors = CardDefaults.cardColors(
-                        containerColor = Color(0xFFffc72a),
-                    ),
-                    modifier = Modifier
-                        .size(width = 240.dp, height = 100.dp)
-                ) {
-                    Text(
-                        text = "#${index + 1}",
-                        modifier = Modifier
-                            .padding(start = 4.dp,top = 4.dp,end = 0.dp, bottom = 0.dp),
-                        fontSize = 18.sp,
-                        fontWeight = FontWeight.ExtraBold,
-                        color = Color(0xFF0a5640)
-                    )
-                    Text(
-                        text = challenge,
-                        modifier = Modifier
-                            .padding(10.dp),
-                        fontSize = 20.sp,
-                        fontWeight = FontWeight.ExtraBold,
-                        color = Color(0xFF0a5640)
-                    )
-                }
-            }
+        itemsIndexed(challenges) { index, challenge ->
+            Challenge(index = index, title = challenge)
         }
+    }
+}
+
+@Composable
+fun Challenge(index: Int, title: String) {
+    Card(
+        colors = CardDefaults.cardColors(
+            containerColor = Color(0xFFffc72a),
+        ),
+        modifier = Modifier
+            .size(width = 240.dp, height = 100.dp)
+    ) {
+        Text(
+            text = "#${index + 1}",
+            modifier = Modifier
+                .padding(start = 4.dp, top = 4.dp, end = 0.dp, bottom = 0.dp),
+            fontSize = 18.sp,
+            fontWeight = FontWeight.ExtraBold,
+            color = Color(0xFF0a5640)
+        )
+        Text(
+            text = title,
+            modifier = Modifier
+                .padding(10.dp),
+            fontSize = 20.sp,
+            fontWeight = FontWeight.ExtraBold,
+            color = Color(0xFF0a5640)
+        )
     }
 }
--- app/src/main/java/com/example/assignment2/MainActivity.kt
+++ app/src/main/java/com/example/assignment2/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -13,7 +14,9 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.Scaffold
@@ -21,8 +24,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -67,11 +72,23 @@ fun Greeting(name: String, studentID: String, modifier: Modifier) {
         modifier = modifier,
         verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        Image(
+            painter = painterResource(id = R.drawable.logo_main),
+            contentDescription = "NDSU Bison Logo",
+            modifier = Modifier
+                .clip(
+                    RoundedCornerShape(16.dp)
+                )
+                .background(Color(0xFF073926)).padding(7.dp)
+                .size(300.dp)
+                .align(Alignment.CenterHorizontally)
+        )
         Text(
             text = "Name: $name",
             fontSize = 25.sp,
             fontWeight = FontWeight.ExtraBold,
-            color = Color(0xFFffc72a)
+            color = Color(0xFFffc72a),
+            modifier = Modifier.padding(vertical = 20.dp)
         )
         Text(
             text = "Student ID: $studentID",
